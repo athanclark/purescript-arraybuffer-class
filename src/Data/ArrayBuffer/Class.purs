992381d514d5d1cf57a099daeee097ae8fc8f6a4
Athan Clark <athan.clark@gmail.com>
2019-02-26 21:57:22 -0700

    attempting unicode reading, may fail because it needs unsigned 32bit ints...?

--- src/Data/ArrayBuffer/Class.purs
+++ src/Data/ArrayBuffer/Class.purs
@@ -13,11 +13,13 @@ import Data.ArrayBuffer.ArrayBuffer (empty) as AB
 import Data.ArrayBuffer.DataView as DV
 
 import Prelude
-  ((<$>), (>>=), (<<<), pure, Unit, Ordering (..), (<=), otherwise, (<>), show, unit, bind, (+), (/=), map)
+  ( Unit, Ordering (..)
+  , (<$>), (>>=), (<<<), (<=), (<>), (+), (/=), (==), (<)
+  , pure, otherwise, show, unit, bind, map)
 import Data.Maybe (Maybe (..))
-import Data.UInt (fromInt) as UInt
-import Data.Char (toCharCode)
-import Data.Int.Bits ((.|.), (.&.), shr)
+import Data.UInt (fromInt, toInt) as UInt
+import Data.Char (toCharCode, fromCharCode)
+import Data.Int.Bits ((.|.), (.&.), shr, shl, xor)
 import Effect (Effect)
 import Effect.Exception (throw)
 
@@ -125,17 +127,55 @@ guardByteLength x success =
 
 instance decodeArrayBufferUint32BE :: DecodeArrayBuffer Uint32BE where
   readArrayBuffer b o = (map Uint32BE) <$> DV.getUint32be (DV.whole b) o
+instance decodeArrayBufferUint32LE :: DecodeArrayBuffer Uint32LE where
+  readArrayBuffer b o = (map Uint32LE) <$> DV.getUint32le (DV.whole b) o
+instance decodeArrayBufferUint16BE :: DecodeArrayBuffer Uint16BE where
+  readArrayBuffer b o = (map Uint16BE) <$> DV.getUint16be (DV.whole b) o
+instance decodeArrayBufferUint16LE :: DecodeArrayBuffer Uint16LE where
+  readArrayBuffer b o = (map Uint16LE) <$> DV.getUint16le (DV.whole b) o
+instance decodeArrayBufferUint8 :: DecodeArrayBuffer Uint8 where
+  readArrayBuffer b o = (map Uint8) <$> DV.getUint8 (DV.whole b) o
+instance decodeArrayBufferInt32BE :: DecodeArrayBuffer Int32BE where
+  readArrayBuffer b o = (map Int32BE) <$> DV.getInt32be (DV.whole b) o
+instance decodeArrayBufferInt32LE :: DecodeArrayBuffer Int32LE where
+  readArrayBuffer b o = (map Int32LE) <$> DV.getInt32le (DV.whole b) o
+instance decodeArrayBufferInt16BE :: DecodeArrayBuffer Int16BE where
+  readArrayBuffer b o = (map Int16BE) <$> DV.getInt16be (DV.whole b) o
+instance decodeArrayBufferInt16LE :: DecodeArrayBuffer Int16LE where
+  readArrayBuffer b o = (map Int16LE) <$> DV.getInt16le (DV.whole b) o
+instance decodeArrayBufferInt8 :: DecodeArrayBuffer Int8 where
+  readArrayBuffer b o = (map Int8) <$> DV.getInt8 (DV.whole b) o
+instance decodeArrayBufferFloat32BE :: DecodeArrayBuffer Float32BE where
+  readArrayBuffer b o = (map Float32BE) <$> DV.getFloat32be (DV.whole b) o
+instance decodeArrayBufferFloat32LE :: DecodeArrayBuffer Float32LE where
+  readArrayBuffer b o = (map Float32LE) <$> DV.getFloat32le (DV.whole b) o
+instance decodeArrayBufferFloat64BE :: DecodeArrayBuffer Float64BE where
+  readArrayBuffer b o = (map Float64BE) <$> DV.getFloat64be (DV.whole b) o
+instance decodeArrayBufferFloat64LE :: DecodeArrayBuffer Float64LE where
+  readArrayBuffer b o = (map Float64LE) <$> DV.getFloat64le (DV.whole b) o
 
 
 -- * Casual instances
 
 instance encodeArrayBufferUnit :: EncodeArrayBuffer Unit where
   putArrayBuffer b o _ = pure (Just 0)
+instance decodeArrayBufferUnit :: DecodeArrayBuffer Unit where
+  readArrayBuffer b o = pure (Just unit)
 -- | Encodes the boolean into an unsigned word8
 instance encodeArrayBufferBoolean :: EncodeArrayBuffer Boolean where
   putArrayBuffer b o x =
     let v = if x then 1 else 0
     in  putArrayBuffer b o (Uint8 (UInt.fromInt v))
+instance decodeArrayBufferBoolean :: DecodeArrayBuffer Boolean where
+  readArrayBuffer b o =
+    let f mV = case mV of
+          Nothing -> pure Nothing
+          Just (Uint8 v) ->
+            let v' = UInt.toInt v
+            in  if v' == 0 then pure (Just false)
+                  else if v' == 1 then pure (Just true)
+                    else throw ("Not a Boolean ArrayBuffer encoding: " <> show v)
+    in  readArrayBuffer b o >>= f
 instance encodeArrayBufferOrdering :: EncodeArrayBuffer Ordering where
   putArrayBuffer b o x =
     let v = case x of
@@ -143,6 +183,18 @@ instance encodeArrayBufferOrdering :: EncodeArrayBuffer Ordering where
               EQ -> 1
               GT -> 2
     in  putArrayBuffer b o (Uint8 (UInt.fromInt v))
+instance decodeArrayBufferOrdering :: DecodeArrayBuffer Ordering where
+  readArrayBuffer b o =
+    let f mV = case mV of
+          Nothing -> pure Nothing
+          Just (Uint8 v) ->
+            let v' = UInt.toInt v
+            in  case unit of
+                  _ | v' == 0 -> pure (Just LT)
+                    | v' == 1 -> pure (Just EQ)
+                    | v' == 2 -> pure (Just GT)
+                    | otherwise -> throw ("Not a Boolean ArrayBuffer encoding: " <> show v)
+    in  readArrayBuffer b o >>= f
 instance encodeArrayBufferChar :: EncodeArrayBuffer Char where
   putArrayBuffer b o c =
     let v = toCharCode c
@@ -197,6 +249,36 @@ instance encodeArrayBufferChar :: EncodeArrayBuffer Char where
                             Just written''' ->
                               pure (Just (written + written' + written'' + written'''))
             | otherwise -> throw ("Char not in unicode range: " <> show c)
+instance decodeArrayBufferChar :: DecodeArrayBuffer Char where
+  readArrayBuffer b o = do
+    let readNextByte o' = (map (\(Uint8 v) -> UInt.toInt v)) <$> readArrayBuffer b o'
+        shl6 x = shl x 6
+    mW <- readNextByte o
+    case mW of
+      Nothing -> pure Nothing
+      Just w -> do
+        r <-
+          if w < 0x80 then pure w
+            else do
+              mX <- (map (xor 0x80)) <$> readNextByte (o + 1)
+              case mX of
+                Nothing -> throw ("Incorrect unicode char encoding - w: " <> show w)
+                Just x ->
+                  if w < 0xe0 then pure (x .|. shl6 (xor 0xc0 w))
+                    else do
+                      mY <- (map (xor 0x80)) <$> readNextByte (o + 2)
+                      case mY of
+                        Nothing -> throw ("Incorrect unicode char encoding - w: " <> show w <> ", x: " <> show x)
+                        Just y ->
+                          if w < 0xf0 then pure (y .|. shl6 (x .|. shl6 (xor 0xe0 w)))
+                            else do
+                              mZ <- (map (xor 0x80)) <$> readNextByte (o + 3)
+                              case mZ of
+                                Nothing -> throw ("Incorrect unicode char encoding - w: " <> show w <> ", x: " <> show x <> ", y: " <> show y)
+                                Just z -> pure (z .|. shl6 (y .|. shl6 (x .|. shl6 (xor 0xf0 w))))
+        case fromCharCode r of -- FIXME sure this works for Ints, and shouldn't be UInts? It's up to 4 bytes...
+          Nothing -> throw ("Incorrect unicode char encoding: " <> show r)
+          Just c -> pure (Just c)
 
 
 
