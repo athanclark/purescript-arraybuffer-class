6bb924819c0fcd85922c5728ca290c698c8edafd
Athan Clark <athan.clark@gmail.com>
2019-02-27 10:18:02 -0700

    using typed array

--- test/Main.purs
+++ test/Main.purs
@@ -1,32 +1,22 @@
 module Test.Main where
 
 import Data.ArrayBuffer.Class
-
+import Data.ArrayBuffer.Typed.Gen
+  (genUint8, genUint16, genUint32, genInt8, genInt16, genInt32, genFloat32, genFloat64)
 
 import Prelude
 import Data.Maybe (Maybe (..))
 import Data.UInt as UInt
 import Data.UInt.Gen (genUInt)
+import Data.Float32.Gen (chooseFloat32)
+import Data.Int (floor)
 import Effect (Effect)
 import Effect.Console (log)
 import Effect.Unsafe (unsafePerformEffect)
 import Test.QuickCheck (Result (Failed), (===), quickCheckGen)
-import Test.QuickCheck.Gen (Gen)
+import Test.QuickCheck.Gen (Gen, chooseInt, choose)
 import Math (pow)
 
-
-
-genU :: Number -> Gen UInt.UInt
-genU high = genUInt (UInt.fromInt 0) (UInt.fromNumber high)
-
-genUint8 :: Gen UInt.UInt
-genUint8 = genU ((pow 2.0 8.0) - 1.0)
-
-genUint16 :: Gen UInt.UInt
-genUint16 = genU ((pow 2.0 16.0) - 1.0)
-
-genUint32 :: Gen UInt.UInt
-genUint32 = genU ((pow 2.0 32.0) - 1.0)
 
 
 main :: Effect Unit
@@ -42,6 +32,24 @@ main = do
   quickCheckGen ((arrayBufferIso <<< Uint16LE) <$> genUint16)
   log "  Uint8:"
   quickCheckGen ((arrayBufferIso <<< Uint8) <$> genUint8)
+  log "  Int32BE:"
+  quickCheckGen ((arrayBufferIso <<< Int32BE) <$> genInt32)
+  log "  Int32LE:"
+  quickCheckGen ((arrayBufferIso <<< Int32LE) <$> genInt32)
+  log "  Int16BE:"
+  quickCheckGen ((arrayBufferIso <<< Int16BE) <$> genInt16)
+  log "  Int16LE:"
+  quickCheckGen ((arrayBufferIso <<< Int16LE) <$> genInt16)
+  log "  Int8:"
+  quickCheckGen ((arrayBufferIso <<< Int8) <$> genInt8)
+  log "  Float32BE:"
+  quickCheckGen ((arrayBufferIso <<< Float32BE) <$> genFloat32)
+  log "  Float32LE:"
+  quickCheckGen ((arrayBufferIso <<< Float32LE) <$> genFloat32)
+  log "  Float64BE:"
+  quickCheckGen ((arrayBufferIso <<< Float64BE) <$> genFloat64)
+  log "  Float64LE:"
+  quickCheckGen ((arrayBufferIso <<< Float64LE) <$> genFloat64)
 
 
 
