d0a3573842e63a63369931edc05ddd5951595317
Athan Clark <athan.clark@gmail.com>
2019-11-16 12:40:25 -0800

    enum instances

--- src/Data/ArrayBuffer/Class/Types.purs
+++ src/Data/ArrayBuffer/Class/Types.purs
@@ -4,7 +4,9 @@ module Data.ArrayBuffer.Class.Types where
 
 import Prelude
   ( class Eq, class Ord, class Show, class Semiring, class Ring, class CommutativeRing
-  , class EuclideanRing, class DivisionRing, class Bounded, negate, (-))
+  , class EuclideanRing, class DivisionRing, class Bounded, negate, (-), (<$>), top, bottom, (<), (>))
+import Data.Maybe (Maybe (..))
+import Data.Enum (class Enum, succ, pred)
 import Data.UInt (UInt, fromInt)
 import Data.Float32 (Float32)
 import Data.Int (pow) as Int
@@ -13,6 +15,7 @@ newtype Uint32BE  = Uint32BE UInt
 derive newtype instance eqUint32BE :: Eq Uint32BE
 derive newtype instance ordUint32BE :: Ord Uint32BE
 derive newtype instance boundedUint32BE :: Bounded Uint32BE
+derive newtype instance enumUint32BE :: Enum Uint32BE
 derive newtype instance showUint32BE :: Show Uint32BE
 derive newtype instance semiringUint32BE :: Semiring Uint32BE
 derive newtype instance ringUint32BE :: Ring Uint32BE
@@ -22,6 +25,7 @@ newtype Uint32LE  = Uint32LE UInt
 derive newtype instance eqUint32LE :: Eq Uint32LE
 derive newtype instance ordUint32LE :: Ord Uint32LE
 derive newtype instance boundedUint32LE :: Bounded Uint32LE
+derive newtype instance enumUint32LE :: Enum Uint32LE
 derive newtype instance showUint32LE :: Show Uint32LE
 derive newtype instance semiringUint32LE :: Semiring Uint32LE
 derive newtype instance ringUint32LE :: Ring Uint32LE
@@ -33,6 +37,9 @@ derive newtype instance ordUint16BE :: Ord Uint16BE
 instance boundedUint16BE :: Bounded Uint16BE where
   top = Uint16BE (fromInt ((Int.pow 2 16)-1))
   bottom = Uint16BE (fromInt 0)
+instance enumUint16BE :: Enum Uint16BE where
+  succ x@(Uint16BE x') = if x < top then Uint16BE <$> succ x' else Nothing
+  pred x@(Uint16BE x') = if x > bottom then Uint16BE <$> pred x' else Nothing
 derive newtype instance showUint16BE :: Show Uint16BE
 derive newtype instance semiringUint16BE :: Semiring Uint16BE
 derive newtype instance ringUint16BE :: Ring Uint16BE
@@ -44,6 +51,9 @@ derive newtype instance ordUint16LE :: Ord Uint16LE
 instance boundedUint16LE :: Bounded Uint16LE where
   top = Uint16LE (fromInt ((Int.pow 2 16)-1))
   bottom = Uint16LE (fromInt 0)
+instance enumUint16LE :: Enum Uint16LE where
+  succ x@(Uint16LE x') = if x < top then Uint16LE <$> succ x' else Nothing
+  pred x@(Uint16LE x') = if x > bottom then Uint16LE <$> pred x' else Nothing
 derive newtype instance showUint16LE :: Show Uint16LE
 derive newtype instance semiringUint16LE :: Semiring Uint16LE
 derive newtype instance ringUint16LE :: Ring Uint16LE
@@ -55,6 +65,9 @@ derive newtype instance ordUint8 :: Ord Uint8
 instance boundedUint8 :: Bounded Uint8 where
   top = Uint8 (fromInt ((Int.pow 2 8)-1))
   bottom = Uint8 (fromInt 0)
+instance enumUint8 :: Enum Uint8 where
+  succ x@(Uint8 x') = if x < top then Uint8 <$> succ x' else Nothing
+  pred x@(Uint8 x') = if x > bottom then Uint8 <$> pred x' else Nothing
 derive newtype instance showUint8 :: Show Uint8
 derive newtype instance semiringUint8 :: Semiring Uint8
 derive newtype instance ringUint8 :: Ring Uint8
@@ -64,6 +77,7 @@ newtype Int32BE  = Int32BE Int
 derive newtype instance eqInt32BE :: Eq Int32BE
 derive newtype instance ordInt32BE :: Ord Int32BE
 derive newtype instance boundedInt32BE :: Bounded Int32BE
+derive newtype instance enumInt32BE :: Enum Int32BE
 derive newtype instance showInt32BE :: Show Int32BE
 derive newtype instance semiringInt32BE :: Semiring Int32BE
 derive newtype instance ringInt32BE :: Ring Int32BE
@@ -73,6 +87,7 @@ newtype Int32LE  = Int32LE Int
 derive newtype instance eqInt32LE :: Eq Int32LE
 derive newtype instance ordInt32LE :: Ord Int32LE
 derive newtype instance boundedInt32LE :: Bounded Int32LE
+derive newtype instance enumInt32LE :: Enum Int32LE
 derive newtype instance showInt32LE :: Show Int32LE
 derive newtype instance semiringInt32LE :: Semiring Int32LE
 derive newtype instance ringInt32LE :: Ring Int32LE
@@ -84,6 +99,9 @@ derive newtype instance ordInt16BE :: Ord Int16BE
 instance boundedInt16BE :: Bounded Int16BE where
   top = Int16BE ((Int.pow 2 15)-1)
   bottom = Int16BE (negate (Int.pow 2 15))
+instance enumInt16BE :: Enum Int16BE where
+  succ x@(Int16BE x') = if x < top then Int16BE <$> succ x' else Nothing
+  pred x@(Int16BE x') = if x > bottom then Int16BE <$> pred x' else Nothing
 derive newtype instance showInt16BE :: Show Int16BE
 derive newtype instance semiringInt16BE :: Semiring Int16BE
 derive newtype instance ringInt16BE :: Ring Int16BE
@@ -95,6 +113,9 @@ derive newtype instance ordInt16LE :: Ord Int16LE
 instance boundedInt16LE :: Bounded Int16LE where
   top = Int16LE ((Int.pow 2 15)-1)
   bottom = Int16LE (negate (Int.pow 2 15))
+instance enumInt16LE :: Enum Int16LE where
+  succ x@(Int16LE x') = if x < top then Int16LE <$> succ x' else Nothing
+  pred x@(Int16LE x') = if x > bottom then Int16LE <$> pred x' else Nothing
 derive newtype instance showInt16LE :: Show Int16LE
 derive newtype instance semiringInt16LE :: Semiring Int16LE
 derive newtype instance ringInt16LE :: Ring Int16LE
@@ -106,6 +127,9 @@ derive newtype instance ordInt8 :: Ord Int8
 instance boundedInt8 :: Bounded Int8 where
   top = Int8 ((Int.pow 2 7)-1)
   bottom = Int8 (negate (Int.pow 2 7))
+instance enumInt8 :: Enum Int8 where
+  succ x@(Int8 x') = if x < top then Int8 <$> succ x' else Nothing
+  pred x@(Int8 x') = if x > bottom then Int8 <$> pred x' else Nothing
 derive newtype instance showInt8 :: Show Int8
 derive newtype instance semiringInt8 :: Semiring Int8
 derive newtype instance ringInt8 :: Ring Int8
